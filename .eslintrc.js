0a64c094780bcfe5f3855a6486286c887e747b0b
phreemason <mr.omarmason@gmail.com>
2024-06-17 00:07:20 -0400

    add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+    extends: ["expo", "prettier"],
+    plugins: ["prettier"],
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+    },
+    "settings": {
+        "import/resolver": {
+            "alias": {
+                "map": [
+                    ["@", "."]
+                ],
+                "extensions": [".js", ".jsx"]
+            }
+        }
+    }
+};
