bf825040e2f8093f5899f3e098630014c624e2db
phreemason <mr.omarmason@gmail.com>
2024-07-19 09:33:01 -0400

    added new db tables from local db

--- frontend/src/types/database.types.ts
+++ frontend/src/types/database.types.ts
@@ -7,6 +7,31 @@ export type Json =
     | Json[];
 
 export type Database = {
+    graphql_public: {
+        Tables: {
+            [_ in never]: never;
+        };
+        Views: {
+            [_ in never]: never;
+        };
+        Functions: {
+            graphql: {
+                Args: {
+                    operationName?: string;
+                    query?: string;
+                    variables?: Json;
+                    extensions?: Json;
+                };
+                Returns: Json;
+            };
+        };
+        Enums: {
+            [_ in never]: never;
+        };
+        CompositeTypes: {
+            [_ in never]: never;
+        };
+    };
     public: {
         Tables: {
             authors: {
@@ -63,7 +88,7 @@ export type Database = {
                         isOneToOne: false;
                         referencedRelation: 'books';
                         referencedColumns: ['id'];
-                    }
+                    },
                 ];
             };
             book_genres: {
@@ -102,7 +127,7 @@ export type Database = {
                         isOneToOne: false;
                         referencedRelation: 'genres';
                         referencedColumns: ['id'];
-                    }
+                    },
                 ];
             };
             book_tags: {
@@ -138,7 +163,7 @@ export type Database = {
                         isOneToOne: false;
                         referencedRelation: 'tags';
                         referencedColumns: ['id'];
-                    }
+                    },
                 ];
             };
             books: {
@@ -210,24 +235,24 @@ export type Database = {
                     created_at: string;
                     id: number;
                     user_book_id: number | null;
-                }
+                };
                 Insert: {
                     bookshelf_id?: number | null;
                     created_at?: string;
                     id?: number;
                     user_book_id?: number | null;
-                }
+                };
                 Update: {
                     bookshelf_id?: number | null;
                     created_at?: string;
                     id?: number;
                     user_book_id?: number | null;
-                }
+                };
                 Relationships: [
                     {
                         foreignKeyName: 'bookshelf_books_bookshelf_id_fkey';
                         columns: ['bookshelf_id'];
-                        isOneToOne: false
+                        isOneToOne: false;
                         referencedRelation: 'bookshelves';
                         referencedColumns: ['id'];
                     },
@@ -238,27 +263,27 @@ export type Database = {
                         referencedRelation: 'user_books';
                         referencedColumns: ['id'];
                     },
-                ]
-            }
+                ];
+            };
             bookshelves: {
                 Row: {
                     created_at: string;
                     id: number;
                     name: string;
                     user_id: string;
-                }
+                };
                 Insert: {
                     created_at?: string;
                     id?: number;
                     name: string;
                     user_id: string;
-                }
+                };
                 Update: {
                     created_at?: string;
                     id?: number;
                     name?: string;
                     user_id?: string;
-                }
+                };
                 Relationships: [
                     {
                         foreignKeyName: 'bookshelves_user_id_fkey';
@@ -319,7 +344,7 @@ export type Database = {
                         isOneToOne: true;
                         referencedRelation: 'users';
                         referencedColumns: ['id'];
-                    }
+                    },
                 ];
             };
             tags: {
@@ -353,7 +378,7 @@ export type Database = {
                     owned_copies: number | null;
                     read_count: number | null;
                     user_id: string;
-                }
+                };
                 Insert: {
                     book_id: number;
                     created_at?: string;
@@ -366,7 +391,7 @@ export type Database = {
                     owned_copies?: number | null;
                     read_count?: number | null;
                     user_id: string;
-                }
+                };
                 Update: {
                     book_id?: number;
                     created_at?: string;
@@ -403,19 +428,19 @@ export type Database = {
                     created_at: string;
                     id: number;
                     user_id: string;
-                }
+                };
                 Insert: {
                     book_id: number;
                     created_at?: string;
                     id?: number;
                     user_id: string;
-                }
+                };
                 Update: {
                     book_id?: number;
                     created_at?: string;
                     id?: number;
                     user_id?: string;
-                }
+                };
                 Relationships: [
                     {
                         foreignKeyName: 'user_scans_book_id_fkey';
@@ -430,7 +455,7 @@ export type Database = {
                         isOneToOne: false;
                         referencedRelation: 'profiles';
                         referencedColumns: ['id'];
-                    }
+                    },
                 ];
             };
         };
@@ -439,6 +464,315 @@ export type Database = {
         };
         Functions: {
             [_ in never]: never;
+        };
+        Enums: {
+            [_ in never]: never;
+        };
+        CompositeTypes: {
+            [_ in never]: never;
+        };
+    };
+    storage: {
+        Tables: {
+            buckets: {
+                Row: {
+                    allowed_mime_types: string[] | null;
+                    avif_autodetection: boolean | null;
+                    created_at: string | null;
+                    file_size_limit: number | null;
+                    id: string;
+                    name: string;
+                    owner: string | null;
+                    owner_id: string | null;
+                    public: boolean | null;
+                    updated_at: string | null;
+                };
+                Insert: {
+                    allowed_mime_types?: string[] | null;
+                    avif_autodetection?: boolean | null;
+                    created_at?: string | null;
+                    file_size_limit?: number | null;
+                    id: string;
+                    name: string;
+                    owner?: string | null;
+                    owner_id?: string | null;
+                    public?: boolean | null;
+                    updated_at?: string | null;
+                };
+                Update: {
+                    allowed_mime_types?: string[] | null;
+                    avif_autodetection?: boolean | null;
+                    created_at?: string | null;
+                    file_size_limit?: number | null;
+                    id?: string;
+                    name?: string;
+                    owner?: string | null;
+                    owner_id?: string | null;
+                    public?: boolean | null;
+                    updated_at?: string | null;
+                };
+                Relationships: [];
+            };
+            migrations: {
+                Row: {
+                    executed_at: string | null;
+                    hash: string;
+                    id: number;
+                    name: string;
+                };
+                Insert: {
+                    executed_at?: string | null;
+                    hash: string;
+                    id: number;
+                    name: string;
+                };
+                Update: {
+                    executed_at?: string | null;
+                    hash?: string;
+                    id?: number;
+                    name?: string;
+                };
+                Relationships: [];
+            };
+            objects: {
+                Row: {
+                    bucket_id: string | null;
+                    created_at: string | null;
+                    id: string;
+                    last_accessed_at: string | null;
+                    metadata: Json | null;
+                    name: string | null;
+                    owner: string | null;
+                    owner_id: string | null;
+                    path_tokens: string[] | null;
+                    updated_at: string | null;
+                    version: string | null;
+                };
+                Insert: {
+                    bucket_id?: string | null;
+                    created_at?: string | null;
+                    id?: string;
+                    last_accessed_at?: string | null;
+                    metadata?: Json | null;
+                    name?: string | null;
+                    owner?: string | null;
+                    owner_id?: string | null;
+                    path_tokens?: string[] | null;
+                    updated_at?: string | null;
+                    version?: string | null;
+                };
+                Update: {
+                    bucket_id?: string | null;
+                    created_at?: string | null;
+                    id?: string;
+                    last_accessed_at?: string | null;
+                    metadata?: Json | null;
+                    name?: string | null;
+                    owner?: string | null;
+                    owner_id?: string | null;
+                    path_tokens?: string[] | null;
+                    updated_at?: string | null;
+                    version?: string | null;
+                };
+                Relationships: [
+                    {
+                        foreignKeyName: 'objects_bucketId_fkey';
+                        columns: ['bucket_id'];
+                        isOneToOne: false;
+                        referencedRelation: 'buckets';
+                        referencedColumns: ['id'];
+                    },
+                ];
+            };
+            s3_multipart_uploads: {
+                Row: {
+                    bucket_id: string;
+                    created_at: string;
+                    id: string;
+                    in_progress_size: number;
+                    key: string;
+                    owner_id: string | null;
+                    upload_signature: string;
+                    version: string;
+                };
+                Insert: {
+                    bucket_id: string;
+                    created_at?: string;
+                    id: string;
+                    in_progress_size?: number;
+                    key: string;
+                    owner_id?: string | null;
+                    upload_signature: string;
+                    version: string;
+                };
+                Update: {
+                    bucket_id?: string;
+                    created_at?: string;
+                    id?: string;
+                    in_progress_size?: number;
+                    key?: string;
+                    owner_id?: string | null;
+                    upload_signature?: string;
+                    version?: string;
+                };
+                Relationships: [
+                    {
+                        foreignKeyName: 's3_multipart_uploads_bucket_id_fkey';
+                        columns: ['bucket_id'];
+                        isOneToOne: false;
+                        referencedRelation: 'buckets';
+                        referencedColumns: ['id'];
+                    },
+                ];
+            };
+            s3_multipart_uploads_parts: {
+                Row: {
+                    bucket_id: string;
+                    created_at: string;
+                    etag: string;
+                    id: string;
+                    key: string;
+                    owner_id: string | null;
+                    part_number: number;
+                    size: number;
+                    upload_id: string;
+                    version: string;
+                };
+                Insert: {
+                    bucket_id: string;
+                    created_at?: string;
+                    etag: string;
+                    id?: string;
+                    key: string;
+                    owner_id?: string | null;
+                    part_number: number;
+                    size?: number;
+                    upload_id: string;
+                    version: string;
+                };
+                Update: {
+                    bucket_id?: string;
+                    created_at?: string;
+                    etag?: string;
+                    id?: string;
+                    key?: string;
+                    owner_id?: string | null;
+                    part_number?: number;
+                    size?: number;
+                    upload_id?: string;
+                    version?: string;
+                };
+                Relationships: [
+                    {
+                        foreignKeyName: 's3_multipart_uploads_parts_bucket_id_fkey';
+                        columns: ['bucket_id'];
+                        isOneToOne: false;
+                        referencedRelation: 'buckets';
+                        referencedColumns: ['id'];
+                    },
+                    {
+                        foreignKeyName: 's3_multipart_uploads_parts_upload_id_fkey';
+                        columns: ['upload_id'];
+                        isOneToOne: false;
+                        referencedRelation: 's3_multipart_uploads';
+                        referencedColumns: ['id'];
+                    },
+                ];
+            };
+        };
+        Views: {
+            [_ in never]: never;
+        };
+        Functions: {
+            can_insert_object: {
+                Args: {
+                    bucketid: string;
+                    name: string;
+                    owner: string;
+                    metadata: Json;
+                };
+                Returns: undefined;
+            };
+            extension: {
+                Args: {
+                    name: string;
+                };
+                Returns: string;
+            };
+            filename: {
+                Args: {
+                    name: string;
+                };
+                Returns: string;
+            };
+            foldername: {
+                Args: {
+                    name: string;
+                };
+                Returns: string[];
+            };
+            get_size_by_bucket: {
+                Args: Record<PropertyKey, never>;
+                Returns: {
+                    size: number;
+                    bucket_id: string;
+                }[];
+            };
+            list_multipart_uploads_with_delimiter: {
+                Args: {
+                    bucket_id: string;
+                    prefix_param: string;
+                    delimiter_param: string;
+                    max_keys?: number;
+                    next_key_token?: string;
+                    next_upload_token?: string;
+                };
+                Returns: {
+                    key: string;
+                    id: string;
+                    created_at: string;
+                }[];
+            };
+            list_objects_with_delimiter: {
+                Args: {
+                    bucket_id: string;
+                    prefix_param: string;
+                    delimiter_param: string;
+                    max_keys?: number;
+                    start_after?: string;
+                    next_token?: string;
+                };
+                Returns: {
+                    name: string;
+                    id: string;
+                    metadata: Json;
+                    updated_at: string;
+                }[];
+            };
+            operation: {
+                Args: Record<PropertyKey, never>;
+                Returns: string;
+            };
+            search: {
+                Args: {
+                    prefix: string;
+                    bucketname: string;
+                    limits?: number;
+                    levels?: number;
+                    offsets?: number;
+                    search?: string;
+                    sortcolumn?: string;
+                    sortorder?: string;
+                };
+                Returns: {
+                    name: string;
+                    id: string;
+                    updated_at: string;
+                    created_at: string;
+                    last_accessed_at: string;
+                    metadata: Json;
+                }[];
+            };
         };
         Enums: {
             [_ in never]: never;
@@ -470,11 +804,11 @@ export type Tables<
         PublicSchema['Views'])
     ? (PublicSchema['Tables'] &
         PublicSchema['Views'])[PublicTableNameOrOptions] extends {
-            Row: infer R
+            Row: infer R;
         }
     ? R
     : never
-    : never
+    : never;
 
 export type TablesInsert<
     PublicTableNameOrOptions extends
@@ -485,17 +819,17 @@ export type TablesInsert<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
     ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
-        Insert: infer I
+        Insert: infer I;
     }
     ? I
     : never
     : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
     ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
-        Insert: infer I
+        Insert: infer I;
     }
     ? I
     : never
-    : never
+    : never;
 
 export type TablesUpdate<
     PublicTableNameOrOptions extends
@@ -506,17 +840,17 @@ export type TablesUpdate<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
     ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
-        Update: infer U
+        Update: infer U;
     }
     ? U
     : never
     : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
     ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
-        Update: infer U
+        Update: infer U;
     }
     ? U
     : never
-    : never
+    : never;
 
 export type Enums<
     PublicEnumNameOrOptions extends
@@ -529,4 +863,4 @@ export type Enums<
     ? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
     : PublicEnumNameOrOptions extends keyof PublicSchema['Enums']
     ? PublicSchema['Enums'][PublicEnumNameOrOptions]
-    : never
+    : never;
