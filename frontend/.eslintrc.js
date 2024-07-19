9808a40a497d2431c319bfdeb13125f2b8616898
phreemason <mr.omarmason@gmail.com>
2024-07-19 09:30:32 -0400

    add airbnb linter to project

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,23 +1,54 @@
-/* eslint-disable prettier/prettier */
 module.exports = {
-    extends: ["expo", "prettier"],
-    plugins: ["prettier"],
-    rules: {
-        'prettier/prettier': [
-            'error',
+    extends: [
+        "airbnb",
+        "airbnb/hooks",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "plugin:prettier/recommended"
+    ],
+    plugins: ["@typescript-eslint", "react", "prettier"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module",
+        "project": "./tsconfig.json"
+    },
+    "rules": {
+        "react/react-in-jsx-scope": 0,
+        "import/no-unresolved": 0,
+        "react/jsx-filename-extension": [1, {
+            "extensions": [
+                ".ts",
+                ".tsx"
+            ]
+        }],
+        "prettier/prettier": [
+            "error",
             {
-                endOfLine: 'auto',
-            },
+                "singleQuote": true,
+                "trailingComma": "all",
+                "arrowParens": "avoid",
+                "endOfLine": "auto"
+            }
         ],
+        // "no-use-before-define": 1,
+        "@typescript-eslint/no-use-before-define": ["error"],
+        "import/extensions": ["error", "never"],
+        "react/prop-types": 0,
+        "no-shadow": 0,
+        "@typescript-eslint/no-shadow": ["error"]
     },
     "settings": {
         "import/resolver": {
             "alias": {
                 "map": [
-                    ["@", "./"]
+                    ["@", "./src"]
                 ],
                 "extensions": [".ts", ".tsx"]
             }
         }
-    }
+    },
 };
